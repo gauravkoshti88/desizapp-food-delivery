ddc284ef4dbbcf79d88a8f1d13844f0b142774b6
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-08 02:53:03 +0530

    deploy in github page

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // ensure correct asset paths
-  build: {
-    chunkSizeWarningLimit: 2000,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react')) return 'react'
-            if (id.includes('react-router-dom')) return 'router'
-            if (id.includes('redux')) return 'redux'
-          }
-        }
-      }
-    }
-  }
+  base: '/desizapp-food-delivery',
 })
