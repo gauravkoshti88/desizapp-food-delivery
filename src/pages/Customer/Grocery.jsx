08b2cf2ab4209b156c80248716b161893184bfe0
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-08 01:37:41 +0530

    fix error

--- src/pages/Customer/Grocery.jsx
+++ src/pages/Customer/Grocery.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react'
 import Logo from '../../assets/ZappGrocery.png'
-import Fruits from '../../assets/Grocery/fruits.png'
-import Vegetables from '../../assets/Grocery/vegetables.png'
+import Fruits from '../../assets/Grocery/Fruits.png'
+import Vegetables from '../../assets/Grocery/Vegetables.png'
 import Dairy from '../../assets/Grocery/dairy.png'
-import Bakery from '../../assets/Grocery/bakery.png'
+import Bakery from '../../assets/Grocery/Bakery.png'
 import { useNavigate } from 'react-router-dom'
 
 const categories = [
