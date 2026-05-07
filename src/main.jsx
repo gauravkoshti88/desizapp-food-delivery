eb232169f1b80080932449ea6b80071e21d1bcc2
gauravkoshti88 <gauravkoshti1@gmail.com>
2026-05-08 02:08:56 +0530

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 
 )
